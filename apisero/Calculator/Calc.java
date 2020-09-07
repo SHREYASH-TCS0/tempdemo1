8db30b41682caf1589b409368ffafe33c707c8e8
SHREYASH-TCS0 <68105062+SHREYASH-TCS0@users.noreply.github.com>
2020-09-07 20:02:59 +0530

    new functions added to calcultor java

--- apisero/Calculator/Calc.java
+++ apisero/Calculator/Calc.java
@@ -5,4 +5,19 @@ public class Calc {
 		System.out.print("Sum of "+a+" and "+b+" is: ");
 		return a+b;
 	}
+	
+	public int sub(int a, int b){
+		System.out.print("Difference of "+a+" and "+b+" is: ");
+		return a-b;
+	}
+	
+	public int mult(int a, int b){
+		System.out.print("Product of "+a+" and "+b+" is: ");
+		return a*b;
+	}
+	
+	public int div(int a, int b){
+		System.out.print("Division of "+a+" and "+b+" is: ");
+		return a/b;
+	}
 }
